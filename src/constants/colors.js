67f53a192f9751ecad18f6fb9ddeaed2d56dcfcb
Arlindo Vinicius Amado Nascimento Silva <viniciusamado7@gmail.com>
2022-08-01 16:32:57 -0300

    Router e MUI

---
+++ src/constants/colors.js
@@ -0,0 +1,3 @@
+export const primaryColor = "#e8222e"
+export const secundaryColor = "#000000"
+export const neutralColor = "#b8b8b8"
---
+++ src/pages/TestPage/TestPage.js
@@ -0,0 +1,19 @@
+import React from "react"
+import { Button } from "@mui/material"
+
+const TestPage = () => {
+    return (
+        <div>
+            <p>
+                Teste Page
+            </p>
+            <Button 
+            color="primary"
+            variant="contained">
+                Teste Button
+            </Button>
+        </div>
+    )
+}
+
+export default TestPage
