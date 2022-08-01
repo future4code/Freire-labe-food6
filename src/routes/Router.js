11b56873232ec876aa02bf68efa3eb0a4b5f32fc
Julia Moreira de Santana <juliamorsantana@gmail.com>
2022-08-01 19:43:07 -0300

    feat: iniciei a pagina de Home, adicionei tabs , e componetente de busca e

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route} from "react-router-dom"
 import TestPage from "../pages/TestPage/TestPage"
+import Home from "../pages/TestPage/Home/Home"
 
 const Router = () => {    
     return(
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<TestPage/>}/>
+                <Route path="/" element={<Home/>}/>
             </Routes>
         </BrowserRouter>
     )
