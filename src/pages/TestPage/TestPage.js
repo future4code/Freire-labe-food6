a442e338e1faa418219c133251bfa69f5c259a5f
Arlindo Vinicius Amado Nascimento Silva <viniciusamado7@gmail.com>
2022-08-02 10:09:55 -0300

    Home, Header

--- src/pages/TestPage/TestPage.js
+++ src/pages/TestPage/TestPage.js
@@ -1,9 +1,11 @@
 import React from "react"
 import { Button } from "@mui/material"
+import Header from "../../components/Header/Header"
 
 const TestPage = () => {
     return (
         <div>
+            <Header/>
             <p>
                 Teste Page
             </p>
